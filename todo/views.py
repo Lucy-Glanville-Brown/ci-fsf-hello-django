a8450811a69c724d4813bc8d0e93b8b6da0aeb40
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2022-02-06 17:13:25 +0000

    fixed add new item save error

--- todo/views.py
+++ todo/views.py
@@ -17,7 +17,7 @@ def add_item(request):
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('get_todo_list')
     form = ItemForm()
     context = {
